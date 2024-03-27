746037d74b1436e5c65c51a7cc5b62542b8529fc
ahsn5 <ahsn50071@gmail.com>
2024-03-27 20:23:11 +0200

    end chat screen ui

--- lib/core/utils/app_paths.dart
+++ lib/core/utils/app_paths.dart
@@ -3,4 +3,5 @@ class AppPaths {
   static String onBoardingDoctorLogo = 'assets/images/doctor logo.png';
   static String onBoardingDateLogo = 'assets/images/date logo.png';
   static String onBoardingConnectionLogo = 'assets/images/connection logo.png';
+  static String doctorImage = 'assets/images/doctorImage .png';
 }
